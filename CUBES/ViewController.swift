50a2c24e8b04fa67fe25a359228428cf3f2e531c
Maxim Mitin <maximus405@gmail.com>
2021-11-16 21:17:15 +0300

    init

--- CUBES/ViewController.swift
+++ CUBES/ViewController.swift
@@ -13,62 +13,68 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var minHeight: CGFloat = 0.2
+    var maxHeight: CGFloat = 0.7
+    var minDispersal: CGFloat = -4
+    var maxDispersal: CGFloat = 4
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    
+    @IBAction func addCubeClicked(_ sender: Any) {
+        generateCube()
     }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    //MARK: - Randomizers for objects
+    
+    func generateRandomVector() -> SCNVector3 {
+        return SCNVector3(CGFloat.random(in: minDispersal...maxDispersal), CGFloat.random(in: minDispersal...maxDispersal), CGFloat.random(in: minDispersal...maxDispersal))
+    }
+    
+    func generateRandomColor() -> UIColor {
+        return UIColor(red: CGFloat.random(in: 0...1),
+                       green: CGFloat.random(in: 0...1),
+                       blue: CGFloat.random(in: 0...1),
+                       alpha: CGFloat.random(in: 0.4...1))
+    }
+    
+    func generateRandomSize() -> CGFloat {
+        return CGFloat.random(in: minHeight...maxHeight)
+    }
+    
+    func generateCube() {
+        let size = generateRandomSize()
+        let cube = SCNBox(width: size, height: size, length: size, chamferRadius: 0.03)
+        
+        cube.materials.first?.diffuse.contents = generateRandomColor()
+        
+        let cubeNode = SCNNode(geometry: cube)
+        cubeNode.position = generateRandomVector()
+        
+        let rotateAction = SCNAction.rotateBy(x: 0, y: 2 * .pi, z: 0, duration: 3)
+        let repeatAction = SCNAction.repeatForever(rotateAction)
+        cubeNode.runAction(repeatAction)
+        
+        sceneView.scene.rootNode.addChildNode(cubeNode)
         
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
